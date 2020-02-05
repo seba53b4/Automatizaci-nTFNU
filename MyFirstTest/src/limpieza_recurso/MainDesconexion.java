5e1cb1e05d80781607848535fa5cfd8cd12a6d14
ipupo <iripleyva@gmail.com>
2020-02-05 11:55:32 -0300

    Añadida Clase Main y Logeo

---
+++ MyFirstTest/src/limpieza_recurso/MainDesconexion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package limpieza_recurso;
+
+/**
+ *
+ * @author ipupo
+ */
+public class MainDesconexion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Proceso_Desconexion pd = new Proceso_Desconexion();
+        pd.desconectar();
+    }
+    
+}
--- MyFirstTest/src/limpieza_recurso/Proceso_Desconexion.java
+++ MyFirstTest/src/limpieza_recurso/Proceso_Desconexion.java
@@ -3,7 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package limpieza_recurso;
+
+import Base.BasePage;
+import org.junit.Test;
+import pages.LoginPage;
 
 /**
  *
@@ -11,4 +16,15 @@ package limpieza_recurso;
  */
 public class Proceso_Desconexion {
     
+    public Proceso_Desconexion()
+    {
+        BasePage.initBaseTest();
+    }
+    public void desconectar(){
+    
+      LoginPage lp = new LoginPage();
+      lp.visit("https://noprd-jit-toms.temu.com.uy:7002/");
+      lp.Nav();
+      lp.signIn();
+    }
 }
